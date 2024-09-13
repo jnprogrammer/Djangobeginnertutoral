7861c7fcaac6b7f1c77183eb133dd98602d0e9b8
Josh <joshuanparker@gmail.com>
2024-09-12 21:14:57 -0400

    Picking this back up.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.template import loader
-
+from django.shortcuts import render
+from django.http import Http404
 from .models import Question
+from django.shortcuts import get_object_or_404, render
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
@@ -23,3 +25,10 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
